7b7b86734f96f356c2fa325665aba1f0e88f7df7
Omnicronical <hassall.o@etoncollege.org.uk>
2019-11-19 13:16:15 +0000

    First

---
+++ Tamagotchi/Tamagotchi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tamagotchi
+//
+//  Created by Hassall, Oscar (HWTA) on 19/11/2019.
+//  Copyright © 2019 Hassall, Oscar (HWTA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Tamagotchi/TamagotchiTests/TamagotchiTests.swift
@@ -0,0 +1,21 @@
+//
+//  TamagotchiTests.swift
+//  TamagotchiTests
+//
+//  Created by Hassall, Oscar (HWTA) on 19/11/2019.
+//  Copyright © 2019 Hassall, Oscar (HWTA). All rights reserved.
+//
+
+import XCTest
+
+class TamagotchiTests: XCTestCase {
+
+    func testTamagotchiWithNoArguementsReturnsNonNilObjects() {
+        //arrange
+        
+        //act
+        let tamagotchi = Tamxsagotchi()
+        //assert
+        XCTAssertNotNil(tamagotchi)
+    }
+}
