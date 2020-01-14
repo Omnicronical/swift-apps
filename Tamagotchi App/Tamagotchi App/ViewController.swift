56c5201b9c3500957e5ae6c32d3a531fcb9c4440
Omnicronical <hassall.o@etoncollege.org.uk>
2020-01-14 12:22:38 +0000

    Beginnings

---
+++ Tamagotchi App/Tamagotchi App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamagotchi App
+//
+//  Created by Hassall, Oscar (HWTA) on 14/01/2020.
+//  Copyright © 2020 Hassall, Oscar (HWTA). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tamagotchi App/Tamagotchi App/statistics.swift
@@ -0,0 +1,9 @@
+//
+//  statistics.swift
+//  Tamagotchi App
+//
+//  Created by Hassall, Oscar (HWTA) on 14/01/2020.
+//  Copyright © 2020 Hassall, Oscar (HWTA). All rights reserved.
+//
+
+import Foundation
